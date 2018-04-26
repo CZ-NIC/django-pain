12f7634cbb211c80279c23bf845960298c428d39
Jan Musílek <jan.musilek@nic.cz>
2018-04-26 10:14:51 +0200

    Setup package for PyPI

--- django_pain/__init__.py
+++ django_pain/__init__.py
@@ -1,3 +1,3 @@
 """Django application for managing bank payments and invoices."""
 
-__version__ = '0.0'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """Setup script for django_pain."""
+import os
+
 from setuptools import find_packages, setup
 
 import django_pain
+
+
+def readme():
+    """Return content of README file."""
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+        return f.read()
+
 
 INSTALL_REQUIRES = ['Django>=2.0', 'django-money']
 EXTRAS_REQUIRE = {'quality': ['isort', 'flake8', 'pydocstyle', 'polint']}
@@ -10,6 +19,25 @@ EXTRAS_REQUIRE = {'quality': ['isort', 'flake8', 'pydocstyle', 'polint']}
 setup(name='django-pain',
       version=django_pain.__version__,
       description='Django application for managing bank payments and invoices',
+      long_description=readme(),
+      url='https://github.com/stinovlas/django-pain',
+      author='Jan Musílek',
+      author_email='jan.musilek@nic.cz',
       packages=find_packages(),
       install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+      extras_require=EXTRAS_REQUIRE,
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      ])
