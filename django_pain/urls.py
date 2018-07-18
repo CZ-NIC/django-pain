e75961d092cda3b290e2a3a55e3874641293aed8
Jan Musílek <jan.musilek@nic.cz>
2018-07-18 10:38:08 +0200

    Issue #21 - Remove view PaymentListView

--- django_pain/urls.py
+++ django_pain/urls.py
@@ -1,9 +1,6 @@
 """django_pain url dispatcher."""
 from django.urls import path
 
-from .views import PaymentListView
-
 app_name = 'pain'
 urlpatterns = [
-    path('payments/', PaymentListView.as_view(), name='payments-list'),
 ]
