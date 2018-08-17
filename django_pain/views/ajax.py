6757b9dfba51ccf2b051e72742f1ea84aa9e5d2b
Jan Musílek <jan.musilek@nic.cz>
2018-08-17 13:39:33 +0200

    Ticket #22462 - Use JsonResponse for ajax view

--- django_pain/views/ajax.py
+++ django_pain/views/ajax.py
@@ -1,7 +1,5 @@
 """AJAX helper views."""
-import json
-
-from django.http import Http404, HttpResponse
+from django.http import Http404, JsonResponse
 
 from django_pain.processors import get_processor_instance
 
@@ -10,6 +8,6 @@ def load_processor_client_choices(request):
     """Load client choices from the appropriate payment processor."""
     try:
         processor = get_processor_instance(request.GET['processor'])
-        return HttpResponse(json.dumps(processor.get_client_choices()))
+        return JsonResponse(processor.get_client_choices())
     except (KeyError, ImportError, AttributeError):
         raise Http404
