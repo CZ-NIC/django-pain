631f5edbd08a55f1ed55e2fb9b6dc3a5af3183d8
Tomas Divis <tomas.divis@nic.cz>
2020-08-12 13:08:43 +0200

    Bump version: 2.0.0-rc5 → 2.0.0-rc6

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ EXTRAS_REQUIRE = {'quality': ['isort', 'flake8', 'pydocstyle', 'polint', 'mypy']
                   'test': ['testfixtures', 'freezegun']}
 
 setup(name='django-pain',
-      version='2.0.0-rc5',
+      version='2.0.0-rc6',
       description='Django application for managing bank payments and invoices',
       long_description=readme(),
       url='http://www.nic.cz/',
